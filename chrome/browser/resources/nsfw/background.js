6aa639366cac3b08e066ad4e220b9faa8b31317b
Elbek Khoshimjonov <elbek.khoshimjonov@atomicvest.com>
2024-04-07 22:46:30 -0400

    update extension

--- chrome/browser/resources/nsfw/background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onCreated.addListener(function (tab) {
-    chrome.extension.getBackgroundPage().console.log('onCreated');
-    chrome.tabs.update(tab.id, { url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley' });
-})
-
-// When user went to new url in same domain
-chrome.tabs.onUpdated.addListener(function(tab){
-    chrome.extension.getBackgroundPage().console.log('onUpdated');
-});
